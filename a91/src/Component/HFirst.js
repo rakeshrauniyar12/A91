12d64c038a443228ecf64793a5f32eaad2821639
rakeshrauniyar12 <rakeshrauniyara1234@gmail.com>
2024-12-27 18:37:11 +0530

    home page done

--- a91/src/Component/HFirst.js
+++ a91/src/Component/HFirst.js
@@ -6,10 +6,14 @@ import account from "../Asset/Navbar/account.png";
 import logo from "../Asset/Navbar/logo.png";
 import search from "../Asset/Navbar/search.png";
 import h1 from "../Asset/Navbar/h1.png";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { RiCloseLargeFill } from "react-icons/ri";
 import filt from "../Asset/Navbar/filter.png";
 import header from "../Asset/Home/header11.png";
 import partner from "../Asset/Navbar/partner.png";
 import "../Style/Home.css"
+import { useNavigate } from "react-router-dom";
 import h2 from "../Asset/Home/h1.png";
 import locat from "../Asset/Home/Icon/location.png"
 import bed from "../Asset/Home/Icon/bed.png"
@@ -20,10 +24,18 @@ import note from "../Asset/Home/Icon/note.png"
 import rect from "../Asset/Home/Icon/rect.png"
 import shhare from "../Asset/Home/Icon/shhare.png"
 import video from "../Asset/Home/Icon/video.png"
-import Navbar1 from "./Navbar1";
 import Footer1 from "./Footer1";
 
 const HFirst = () => {
+  const navigate = useNavigate();
+   const [isAccount,setAccount] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
+    const [showSignupComponent, setShowSignupComponent] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
+    const [showPurposeDropdown,setShowPurposeDropdown] = useState(false);
+    const [showCityDropdown,setShowCityDropdown] = useState(false);
+    const [showOwnDropdown,setShowOwnDropdown] = useState(false);
+    const [isPurRotate,setIsPurRotate] = useState(false);
   const [isOwnRotate, setIsOwnRotate] = useState(false);
   const [isCityRotate, setIsCityRotate] = useState(false);
   const [showFirstComponent, setShowFirstComponent] = useState(true);
@@ -59,12 +71,51 @@ const HFirst = () => {
   }, []);
 
   const handleCityDropdown = () => {
+    setShowCityDropdown(!showCityDropdown)
     setIsCityRotate(!isCityRotate);
   };
+  const handleAccountOption = ()=>{
+    setAccount(!isAccount)
+}
+const handleLoginClick = () => {
+ setShowLogin(!showLogin);
+ setAccount(false)
+};
+const closeSignupComponent = ()=>{
+ setShowSignupComponent(false);
+}
 
-  const handleOwnDropdown = () => {
-    setIsOwnRotate(!isOwnRotate);
-  };
+const closeLoginComponent = ()=>{
+ setShowLogin(false);
+}
+  const handleLoginForm= ()=>{
+   console.log("handleLoginForm",showLogin)
+   console.log("handleLoginForm",showSignupComponent)
+   setShowSignupComponent(false)
+
+   setShowLogin(true);
+  }
+  const handleSignupForm= ()=>{
+   console.log("handleSignupForm",showLogin)
+   console.log("handleSignupForm",showSignupComponent)
+   setShowSignupComponent(true);
+   setAccount(false);
+  }
+  const handlePurposeDropdown = ()=>{
+     setShowPurposeDropdown(!showPurposeDropdown);
+     setIsPurRotate(!isPurRotate)
+  }
+  
+
+const handleOwnDropdown = ()=>{
+ setShowOwnDropdown(!showOwnDropdown);
+ setIsOwnRotate(!isOwnRotate)
+}
+const goToDetailPage = ()=>{
+setAccount(false);
+navigate("/detailpage");
+}
+
  const products = [];
   for(let r=0;r<6;r++){
     products.push({
@@ -115,10 +166,10 @@ const HFirst = () => {
                           <p className="font-incre">Purpose</p>
                           <RiArrowDropDownLine
                             style={{ 
-                              // transform: isPurRotate ? "rotate(180deg)" : "rotate(0deg)",
-                              // transition: isPurRotate ? "transform 0.5s ease" : "transform 0.5s ease",
+                              transform: isPurRotate ? "rotate(180deg)" : "rotate(0deg)",
+                              transition: isPurRotate ? "transform 0.5s ease" : "transform 0.5s ease",
                               fontSize: "35px", fontWeight: "600", cursor:"pointer" }}
-                            // onClick={handlePurposeDropdown}
+                            onClick={handlePurposeDropdown}
                           />
                         </div>
                         </div>
@@ -133,10 +184,11 @@ const HFirst = () => {
             <div>
               <img src={partner} alt="Partner" />
             </div>
-            <div>
+            <div style={{cursor:"pointer"}} onClick={()=>handleAccountOption()}>
               <img src={account} alt="Account" />
             </div>
           </div>
+          
           </>
           :
           <>
@@ -154,7 +206,7 @@ const HFirst = () => {
           <div>
             <img src={partner} alt="Partner" />
           </div>
-          <div>
+          <div style={{cursor:"pointer"}} onClick={()=>handleAccountOption()}>
             <img src={account} alt="Account" />
           </div>
         </div>
@@ -186,10 +238,10 @@ const HFirst = () => {
                         <p className="font-incre">Purpose</p>
                         <RiArrowDropDownLine
                           style={{ 
-                            // transform: isPurRotate ? "rotate(180deg)" : "rotate(0deg)",
-                            // transition: isPurRotate ? "transform 0.5s ease" : "transform 0.5s ease",
+                            transform: isPurRotate ? "rotate(180deg)" : "rotate(0deg)",
+                            transition: isPurRotate ? "transform 0.5s ease" : "transform 0.5s ease",
                             fontSize: "35px", fontWeight: "600", cursor:"pointer" }}
-                          // onClick={handlePurposeDropdown}
+                          onClick={handlePurposeDropdown}
                         />
                       </div>
                       </div>
@@ -198,7 +250,51 @@ const HFirst = () => {
         </>
         }
             
-          
+            {showCityDropdown && 
+             <div className="city-dropdown-12">
+               <div>
+                   <p>Mumbai</p>
+                   <p>Delhi</p>
+                   <p>Kolkata</p>
+                   <p>Bengaluru</p>
+                   <p>Chennai</p>
+                   <p>Hyderabad</p>
+               </div>
+             </div>
+          }
+          {showOwnDropdown && 
+             <div className="own-dropdown-12">
+               <div>
+                   <p>Own</p>
+                   <p>Rent</p>
+               </div>
+             </div>
+          }
+          {showPurposeDropdown && 
+             <div className="purpose-dropdown-12">
+               <div>
+                   <p onClick={()=>{
+                    setShowPurposeDropdown(false)
+                    setIsPurRotate(!isPurRotate)
+                    navigate("/homedesign")
+                   }}>Residential</p>
+                   <p
+                   onClick={()=>{
+                    setShowPurposeDropdown(false)
+                    setIsPurRotate(!isPurRotate)
+                    navigate("/homedesignfirst")
+                   }}
+                   >Commercial</p>
+                   <p 
+                   onClick={()=>{
+                    setShowPurposeDropdown(false)
+                    setIsPurRotate(!isPurRotate)
+                    navigate("/homedesignsecond")
+                   }}
+                   >Plot</p>
+               </div>
+             </div>
+          } 
          
         </div>
         {showOptionsInNavbar && (
@@ -223,7 +319,148 @@ const HFirst = () => {
             <div className="divide-line-11" style={{marginLeft:"15px"}}></div>
             </div>
         )}
+      {
+            isAccount && 
+            <div className="account-details1">
+               <p className="account-option" onClick={handleLoginClick}>Login</p>
+               <p className="account-option" onClick={handleSignupForm}>Register as Individual</p>
+               <p className="account-option" onClick={goToDetailPage}>Register as Channel Partner</p>
+            </div>
+          
+          }
+         {showLogin && (
+        <>
+          <div
+            className="overlay"
+          
+          ></div>
+          <div className="login-container" >
+          <div className="register-container">
+            <div className="regis-heading">
+            <h1 style={{textAlign:"center"}}>Login</h1>
+            <RiCloseLargeFill style={{fontSize:"25px",cursor:"pointer"}} onClick={closeLoginComponent}/>
+            </div>
+       
 
+        <form>
+           <div className="login-input">
+             {/* <div className="login-first-div">
+                <label style={{fontSize:"12px",color:"#008f3d"}}>Phone Number</label>
+                <select style={{border:"none",backgroundColor:"#d9d9d9"}}>
+                    <option>+91</option>
+                </select>
+               
+             </div> */}
+             <div className="login-second-div">
+                <input placeholder="Enter Your Phone Number / Email ID" style={{border:"none",backgroundColor:"#d9d9d9"}}/>
+             </div>
+             <div className="login-third-div">
+                <button>Send OTP</button>
+             </div>
+            </div>
+               {/* {isMobile? <div className="login-third-div">
+                <button>Send OTP</button>
+             </div>:""} */}
+            <div className="login-input">
+             <div className="login-second-div11">
+            
+                <input placeholder="Enter Otp*" className="input-enter-otp" style={{border:"none",backgroundColor:"#d9d9d9"}} required/>
+             </div>
+             <div className="login-third-div">
+                <button>Verify OTP</button>
+             </div>
+            </div>
+           
+
+           
+
+            
+            <div className="link-ff">
+            <Link className="link-forgot">Forgot Password..?</Link>
+                </div>
+                <div className="regis-input1">
+                  <input type="checkbox" className="regi-inpu"/>
+                  <p>By clicking you agree to <Link className="p-link">Terms and Conditions</Link></p>
+                </div>
+                <button className="reg-btn1">Login</button>
+            </form>
+            <div className="las-footer33">
+                <div>
+                <p style={{fontSize:"14px"}}>Don't have an account?</p>
+                <Link className="las-btn-44" onClick={()=>{handleSignupForm()}}>Register</Link>
+                </div>
+                <Link className="register-link">
+                <div>
+                <p style={{fontSize:"14px"}}>Continue with google</p>
+                <div ><FcGoogle style={{borderRadius:"100px",border:"1px solid black",padding:"2px",fontSize:"20px"}}/></div>
+                </div>
+                </Link>
+                
+            </div>
+            </div> {/* Pass a callback to close after login */}
+          </div>
+        </>
+      )}
+      {showSignupComponent && (
+  <>
+    <div className="overlay1" onClick={() => setShowSignupComponent(false)}></div>
+    <div className="signup-component-container">
+      <div className="register-container">
+        <div className="regis-heading">
+        <h1 style={{textAlign:"center"}}>Register</h1>
+        <RiCloseLargeFill style={{fontSize:"25px",cursor:"pointer"}} onClick={closeSignupComponent}/>
+        </div>
+       
+
+        <form >
+          <div className="regis-form-11">
+
+         
+          <div className="regis-input">
+            <input type="text" placeholder="First Name"/>
+          </div>
+          <div className="regis-input">
+            <input type="text" placeholder="Last Name"/>
+          </div>
+          <div className="regis-input">
+            <input type="email" placeholder="Email ID"/>
+          </div>
+          <div className="regis-input">
+            <input type="number" placeholder="Enter Otp*"/>
+          </div>
+          <div className="regis-input">
+            <input type="number" placeholder="Phone Number"/>
+          </div>
+          <div className="regis-input">
+            <input type="number" placeholder="Enter Otp*"/>
+          </div>
+          </div>
+          <div className="regis-input1">
+            <input type="checkbox" className="regi-inpu"/>
+            <p>By clicking you agree to <Link className="p-link">Terms and Conditions</Link></p>
+          </div>
+          <Link to={"/accountindividualdashboard"}><button className="reg-btn1" onClick={closeSignupComponent}>Register</button></Link>
+        </form>
+
+        <div className="las-footer33">
+          <div>
+            <p>I have an account?</p>
+            <Link className="las-btn-44" onClick={()=>{
+              closeSignupComponent()
+              handleLoginForm()
+              }}>Login</Link>
+          </div>
+          <Link className="register-link">
+            <div>
+              <p>Register with Google</p>
+              <div><FcGoogle style={{borderRadius:"100px",border:"1px solid black",padding:"2px",fontSize:"20px"}}/></div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </>
+)}
         <div className="h-main-content">
           <p
             style={{
